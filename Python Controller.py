8362036b55258699730da710e8b254fbd12909b2
Beh Ming Jun <58068400+behmingjun@users.noreply.github.com>
2022-03-19 22:10:23 +0800

    Update Python Controller.py

--- Python Controller.py
+++ Python Controller.py
@@ -1,8 +1,22 @@
 import serial
 import pyautogui
 import time
+import re
 
-ser = serial.Serial("COM6", 9600)
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+print(volume.GetVolumeRange())
+#volume.SetMasterVolumeLevel(-30, None)
+
+
+ser = serial.Serial("COM8", 9600)
 ser.write(bytes("Start", "utf-8"))
 
 while True:
@@ -12,8 +26,11 @@ while True:
     if "Pause/Play" in data:
         pyautogui.press(['space'], interval = 0)
 
-    if "Volume Up" in data:
-        pyautogui.press(['volumeup'], interval = 0)
-
-    if "Volume Down" in data:
-        pyautogui.press(['volumedown'], interval = 0)
+    if "Volume" in data:
+        dataString = str(data)
+        vol = re.findall("\d+|.\d+", dataString)
+        #print(vol)
+        vol = float(vol[0]+vol[1])
+        #print(vol) 
+        print(f'Setting volume to {(vol + 65.25) / 0.6525}')
+        volume.SetMasterVolumeLevel(vol, None)
