0c4cb4f8e3ac2efc135724809b6237cf20de09e9
Beh Ming Jun <58068400+behmingjun@users.noreply.github.com>
2022-03-19 22:09:57 +0800

    Update Arduino Input Reader.ino

--- Arduino Input Reader.ino
+++ Arduino Input Reader.ino
@@ -1,75 +1,50 @@
-
-// pressure pad variables
-int pPin = A5;
-float pValue = 0;
-
-// slider variables
 int sPin = A0;
-float initialSValue;
-float sValue = 0;
-int initialVolume;
-float sValDiff;
-int counter;
+float sVal0 = 0;
+float sVal1;
+int vol;
+float volSend = 0;
+const float pycawUnit = 0.6525;
+const float sliderUnit = 10.23;
+
+int pPin = A5;
+float pVal = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(sPin, INPUT);
   pinMode(pPin, INPUT);
-  Serial.begin(9600);
+  sVal0 = analogRead(sPin); 
+  vol = (sVal0 / sliderUnit);
+  volSend = -65.25 + (vol * pycawUnit);
 
-//  // initialise the volume
-  initialSValue = analogRead(sPin);
-//  initialVolume = initialSValue / 20.48;
-//  Serial.write(initialVolume);
-//  Serial.write("\n");
-//  for (int i = 0; i < 50; i++){
-//    Serial.write("Volume Down\n");
-//  }
-//  for (int i = 0; i < initialSValue; i++){
-//    Serial.write("Volume Up\n");
-//  }
+  Serial.write("Volume: ");
+  Serial.println(volSend);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  if (Serial.available()){
-//    if (Serial.readString() == "Start"){
-//      setup();      
-//    }
-//  }
+  if (Serial.read() == "Start"){
+    setup();  
+  }
   
-  sValue = analogRead(sPin);
-  pValue = analogRead(pPin);
+  sVal1 = analogRead(sPin);
+  pVal = analogRead(pPin);
+
+//  Serial.println(sVal1);
+//  delay(800);
   
-  if (pValue > 0){
+  if (pVal > 0){
     Serial.write("Pause/Play\n");
-    delay(50);
+    delay(150);
   }
 
-  if (initialSValue > sValue){
-    sValDiff = initialSValue - sValue;
-    counter = int(sValDiff / 20.46);
-    for (int i = 0; i < counter; i++){
-      Serial.write("Volume Down\n");
-    }
-    initialSValue = sValue;
-    delay(100);
+  if (sVal1 > sVal0 + 10 || sVal1 < sVal0 - 10){
+    vol = sVal1 / sliderUnit;
+    volSend = -65.25 + (vol * pycawUnit);
+    Serial.write("Volume: ");
+    Serial.println(volSend);
+    sVal0 = sVal1;
+    delay(100);    
   }
-  
-  else if (initialSValue < sValue){
-    sValDiff = sValue - initialSValue;
-    counter = int(sValDiff / 20.48);
-    for (int i = 0; i < counter; i++){
-      Serial.write("Volume Up\n");
-    }
-    initialSValue = sValue;
-    delay(100);
-  }
-  
-  delay(100);
-  
-//  Serial.print("Value of slider | pressure pad: ");
-//  Serial.print(sValue);
-//  Serial.print(" | ");
-//  Serial.println(pValue);
 }
